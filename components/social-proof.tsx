d3a8dc22c5140eaf6902b9c86c1fab62cccb3124
paullington <57174058+paullington@users.noreply.github.com>
2026-06-24 17:59:22 +0100

    Update social-proof.tsx

--- components/social-proof.tsx
+++ components/social-proof.tsx
@@ -16,6 +16,10 @@ const recentDonors = [
   { name: 'Anonymous', amount: 100_000, time: '21/06/2026' },
   { name: 'Anonymous', amount: 20_000, time: '23/06/2026' },
   { name: 'Anonymous', amount: 1_500, time: '23/06/2026' },
+  { name: 'Anonymous', amount: 20_000, time: '24/06/2026' },
+  { name: 'Anonymous', amount: 20_000, time: '24/06/2026' },
+  { name: 'CHRISTIANA EDIRIN YAVUNADU', amount: 20_000, time: '24/06/2026' },
+  { name: 'ABOSEDE Lota Caldwell', amount: 300_000, time: '24/06/2026' },
 ]
 
 export function SocialProof() {
